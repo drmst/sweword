54094a8e9d99ac0cc80a694a47d7aaa74db946e5
Mesut Öztürk <doktormesut@gmail.com>
2024-09-13 00:38:51 +0300

    json verisine ulaşıyorum

--- src/components/word-container/index.jsx
+++ src/components/word-container/index.jsx
@@ -1,24 +1,35 @@
-import React,{ useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const WordContainer = ({ searchURL }) => {
   const [data, setData] = useState(null);
   useEffect(() => {
     fetch(searchURL)
-    .then((response) =>{
-      if(!response.ok){
-      throw new Error("ağ cevabı hatalı");
-    }
-  return response.json();
-})
-    .then((result) => setData(result))
-    .catch(error=>console.log(error));
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("ağ cevabı hatalı");
+        }
+        return response.json();
+      })
+      .then((result) => setData(result))
+      .catch((error) => console.log(error));
   }, [searchURL]);
 
-  console.log("datam", JSON.stringify(data));
+  /* console.log("datam", JSON.stringify(data)); */
   return (
-  /*   {if(data[0]==="found"){
-      console.log(data)
-    }} */ //burda hata var
+    <div>
+      {data && data.Status === "found" && (
+        <div>
+          {data.Result.map((resultItem, index) => (
+            <div key={index}>
+              <h2>{resultItem.Value}</h2>
+              <h3>{resultItem.Type}</h3>
+             {resultItem.}
+              
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
   );
 };
 
